7583f634bf4e5b302561170bf044b9cebb76262e
Hrant Aralelyan <hrantarakel@gmail.com>
2021-02-25 18:39:07 +0400

    Disable Spring Security logging

--- src/main/java/com/accelerator28/bookeat/BookEatApplication.java
+++ src/main/java/com/accelerator28/bookeat/BookEatApplication.java
@@ -2,8 +2,9 @@ package com.accelerator28.bookeat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Delete "(exclude = SecurityAutoConfiguration.class)" in order to enable Spring Security logging
 public class BookEatApplication {
 
     public static void main(String[] args) {
